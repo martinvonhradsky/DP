0192052f2e4bea8d0538dbaf9be900530ed39df5
Martin <martin.hradsky72@gmail.com>
2024-05-01 11:57:56 +0200

    test add, edit and delete

--- app/api.php
+++ app/api.php
@@ -257,9 +257,8 @@ function deleteUser($alias) {
 
 }
 
-function insertTest($num, $filename, $executable, $description, $local, $name, $technique_id,$args) {
+function insertTest($num, $filename, $executable, $description, $local, $name, $technique_id, $args) {
   // Validate inputs
-  echo "0";
   if (strcasecmp($local, "TRUE") === 0) {
     $local = true;
   } else {
@@ -271,16 +270,36 @@ function insertTest($num, $filename, $executable, $description, $local, $name, $
   if (empty($technique_id)) {
     throw new InvalidArgumentException("Technique ID cannot be empty");
   }
-  echo "1";
+  
   try {
-    echo "2";
     $db = new Database();
     $db->connect();
 
-      echo "3";
-    $stmt = $db->prepare("INSERT INTO tests ( technique_id, test_number, name, executable, local_execution, description, file_name, arguments) VALUES
-     (:technique_id, :test_number, :name, :executable, :local_execution, :description, :file_name, :arguments)");
+    // Check if a test with the same technique_id and test_number already exists
+    $query = "SELECT COUNT(*) AS count FROM tests WHERE technique_id = :technique_id AND test_number = :test_number";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':technique_id', $technique_id);
+    $stmt->bindParam(':test_number', $num);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $count = $result['count'];
 
+    if ($count > 0) {
+      // If the test already exists, update it
+      $query = "UPDATE tests SET 
+                file_name = :file_name, 
+                executable = :executable, 
+                description = :description, 
+                local_execution = :local_execution, 
+                arguments = :arguments 
+                WHERE technique_id = :technique_id AND test_number = :test_number";
+    } else {
+      // If the test doesn't exist, insert it
+      $query = "INSERT INTO tests (technique_id, test_number, name, executable, local_execution, description, file_name, arguments) 
+                VALUES (:technique_id, :test_number, :name, :executable, :local_execution, :description, :file_name, :arguments)";
+    }
+
+    $stmt = $db->prepare($query);
     $stmt->bindParam(':technique_id', $technique_id);
     $stmt->bindParam(':test_number', $num);
     $stmt->bindParam(':name', $name);
@@ -289,9 +308,9 @@ function insertTest($num, $filename, $executable, $description, $local, $name, $
     $stmt->bindParam(':description', $description);
     $stmt->bindParam(':file_name', $filename);
     $stmt->bindParam(':arguments', $args, PDO::PARAM_BOOL);
-    echo "4";
+    
     $result = $stmt->execute();
-    echo "5";
+
     if ($result) {
       return true;
     } else {
@@ -299,10 +318,11 @@ function insertTest($num, $filename, $executable, $description, $local, $name, $
     }
   } catch (PDOException $e) {
     // Log error message
-    error_log("Error inserting test into database: " . $e->getMessage());
+    error_log("Error inserting or updating test into database: " . $e->getMessage());
     return false;
   }
 }
+
 
 function getAnsibleOutput() {
   // Check if the output file exists
@@ -329,60 +349,195 @@ function getAnsibleOutput() {
 
 
 function saveTest($json_data){
+  // Create a new database object
+  $db = new Database();
+  $db->connect();
+
+  // Sanitize and validate input parameters
+  $url = filter_var($json_data->url, FILTER_SANITIZE_URL);
+  $filename = filter_var($json_data->file_name, FILTER_SANITIZE_STRING);
+  $executable = filter_var($json_data->executable, FILTER_SANITIZE_STRING);
+  $description = filter_var($json_data->desc, FILTER_SANITIZE_STRING);
+  $local = filter_var($json_data->local_execution, FILTER_VALIDATE_BOOLEAN);
+  $name = filter_var($json_data->name, FILTER_SANITIZE_STRING);
+  $id = $json_data->technique_id;
+  $args = $json_data->args;
+
+  // Check if ID is valid
+  if(!isID($id)){
+    http_response_code(400);
+    echo json_encode(array("error" => "Invalid ID"));
+    exit;
+  } else {
+    $query = "SELECT COUNT(*) > 0 AS exists FROM mitre WHERE id = '$id'";
+    
+    $result = executeQuery($query);
+    $exists = json_decode($result, true);
+    if(!$exists){
+      http_response_code(400);
+      echo json_encode(array("error" => "ID does not match any technique ID"));
+      exit;
+    }
+  }
+
+  // Validate URL
+  if(!filter_var($url, FILTER_VALIDATE_URL)){
+    http_response_code(400);
+    echo json_encode(array("error" => "Invalid URL"));
+    exit;
+  }
+
+  // Get the maximum test number
+  $query = "SELECT MAX(test_number) FROM tests WHERE technique_id = '$id'";
+  $result = executeQuery($query);
+  $data = json_decode($result, true);
+  $max = (int) $data[0]['max'];
+
+  // Calculate the new test number
+  if($max == null){
+    $num = 1;
+  } else {
+    $num = $max + 1;
+  }
+  // Execute custom test script
+  $command = "../engine/custom_test.sh -i ".$id." -u ".$url." -n ".$num;  
+  // Execute the command with parameters
+  $output = shell_exec($command);
+
+  // Insert the test into the database
+  $query = "INSERT INTO tests (test_number, file_name, executable, description, local_execution, name, technique_id, arguments) 
+            VALUES (:num, :filename, :executable, :description, :local, :name, :id, :args)";
+  $params = array(
+    ':num' => $num,
+    ':filename' => $filename,
+    ':executable' => $executable,
+    ':description' => $description,
+    ':local' => $local,
+    ':name' => $name,
+    ':id' => $id,
+    ':args' => $args
+  );
+  
+  // Execute the prepared statement
+  $stmt = $db->prepare($query);
+  $result = $stmt->execute($params);
+
+  // Check the result and send appropriate response
+  if($result){
+    http_response_code(200);
+    echo json_encode(array("message" => "Test saving was successful"));
+  } else {
+    http_response_code(500);
+    echo json_encode(array("error" => "Test saving failed"));
+  }
+}
+
+function editTest($json_data){
 
   // Create a new database object
   $db = new Database();
   $db->connect();
 
-  $url = $json_data->url;
-  $filename = $json_data->filename;
-  $executable = $json_data->executable;
-  $description = $json_data->desc;
-  $local = $json_data->local;
-  $name = $json_data->name;
-  $id = $json_data->id;
+  // Sanitize and validate input parameters
+  $filename = filter_var($json_data->file_name, FILTER_SANITIZE_STRING);
+  $executable = filter_var($json_data->executable, FILTER_SANITIZE_STRING);
+  $test_number = filter_var($json_data->test_number, FILTER_SANITIZE_NUMBER_INT);
+  $description = filter_var($json_data->desc, FILTER_SANITIZE_STRING);
+  $local = filter_var($json_data->local_execution, FILTER_VALIDATE_BOOLEAN);
+  $name = filter_var($json_data->name, FILTER_SANITIZE_STRING);
+  $id = $json_data->technique_id;
   $args = $json_data->args;
+
+  // Check if ID is valid
   if(!isID($id)){
-    echo "Error: ID is no valid";
-    exit;
-  } else {
-    $query = "SELECT COUNT(*) > 0 AS exists FROM mitre WHERE id = '$id';";
-    $result = executeQuery($query); 
-    if(strpos($result,"false")){
-      echo "Error: ID does not match any technique ID";
-      exit;
-    }
-  }
-  if(!isURLValid($url)){
-    echo "Error: Invalid URL.";
+    http_response_code(400);
+    echo json_encode(array("error" => "Invalid ID"));
     exit;
   }
 
-  $query = "SELECT MAX(test_number) FROM tests WHERE technique_id = '$id';";
-  $result = executeQuery($query);  
-  $data = json_decode($result, true);
-  $max = (int) $data[0]['max'];  
+  // Insert or update the test into the database
+  $query = "UPDATE tests 
+            SET file_name = :filename, executable = :executable, description = :description, local_execution = :local, 
+            name = :name, arguments = :args
+            WHERE technique_id = :id AND test_number = :test_number";
+  $params = array(
+    ':filename' => $filename,
+    ':executable' => $executable,
+    ':description' => $description,
+    ':local' => $local,
+    ':name' => $name,
+    ':args' => $args,
+    ':id' => $id,
+    'test_number' => $test_number
+  );
 
-  if($max == null){
-      $num=1;
-  }else{
-      $num = $max + 1;
-  }
-  
-  $command = "../engine/custom_test.sh -i " . $id . " -u " . $url . " -n " . $num ;
+  // Execute the prepared statement
+  $stmt = $db->prepare($query);
+  $result = $stmt->execute($params);
 
-
-  
-  echo shell_exec($command);
-
-  $result = insertTest($num, $filename, $executable, $description, $local, $name, $id, $args);
-  
+  // Check the result and send appropriate response
   if($result){
-      echo "Test saving was successful.";
-  }else{
-      echo "Test saving failed.";
+    http_response_code(200);
+    echo json_encode(array("message" => "Test edit was successful"));
+  } else {
+    http_response_code(500);
+    echo json_encode(array("error" => "Test edit failed"));
   }
 }
+
+function deleteTest($tech, $test){
+  try {
+    // Create a new database object
+    $db = new Database();
+    $db->connect();
+      
+    // Prepare the delete statement with placeholders
+    $sql = "DELETE FROM tests WHERE technique_id = :technique_id AND test_number = :test_number";
+
+    $stmt = $db->prepare($sql);
+    $params = array(
+      ':technique_id' => $tech,
+      ':test_number' => $test
+    );
+
+    // Execute the statement
+    $stmt->execute($params);
+
+    // Check if any rows were affected
+    if ($stmt->rowCount() > 0) {
+      // Deletion successful, now delete the corresponding folder
+      $folder_path = "../engine/customs/$tech/$test"; 
+      $delete_script_path = "../engine/delete_test.sh"; 
+
+      // Execute the delete_test.sh script
+      $output = shell_exec("$delete_script_path $folder_path");
+
+      // Check if deletion was successful
+      if (strpos($output, "Folder $folder_path and its contents deleted successfully") !== false) {
+        http_response_code(200); // Success
+        return true; // Deletion successful
+      } else {
+        http_response_code(500); // Server error
+        return false; // Deletion failed
+      }
+    } else {
+      http_response_code(404); // Not found
+      return false; // No rows were affected, test not found
+    }
+
+    $db = null; // Close connection
+  } catch (PDOException $e) {
+    // Log error message
+    error_log("Error deleting test from database: " . $e->getMessage());
+    http_response_code(500); // Server error
+    return false;
+  }
+}
+
+
+
+########################################################
+###     Main loop
 
 if (isset($_GET['action'])) {
   switch ($_GET['action']) {
@@ -532,6 +687,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       case 'test': 
         saveTest($json_data);
         break;
+      case 'edit_test':
+        editTest($json_data);
+        break;
       default:
         echo "Unknown action";
         break;
@@ -553,6 +711,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
           deleteUser($alias);
         } else {
           echo "Alias parameter is missing";
+        }
+        break;
+      case 'test':
+        if(isset($json_data->technique_id) && isset($json_data->test_number)){
+          
+          deleteTest($json_data->technique_id, $json_data->test_number);
         }
         break;
       default:
