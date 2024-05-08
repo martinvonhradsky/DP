428dbdfea00824b245a752d8d4fde2bc211bba2e
martinvonhradsky <martin.hradsky72@gmail.com>
2024-05-08 15:51:06 +0200

    moved to customTest

--- client/src/components/CustomTest.vue
+++ client/src/components/CustomTest.vue
@@ -79,6 +79,7 @@
             </table>
           </div>
         </div>
+        <EasyDataTable class="test" :headers="headers" :items="items" />
       </div>
     </div>
     <Modal v-model="isShow" :close="closeModal">
@@ -114,9 +115,60 @@
 import { onMounted, ref } from "vue";
 import FormCustomTest from "./shared/FormCustomTest.vue";
 import { useTestStore } from "../store/testStore";
+import EasyDataTable from "vue3-easy-data-table";
 
 const store = useTestStore();
 const isShow = ref(false);
+
+const headers = [
+  { text: "PLAYER", value: "player" },
+  { text: "TEAM", value: "team" },
+  { text: "NUMBER", value: "number" },
+  { text: "POSITION", value: "position" },
+  { text: "HEIGHT", value: "indicator.height" },
+  { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
+  { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
+  { text: "COUNTRY", value: "country" },
+];
+
+const items = [
+  {
+    player: "Stephen Curry",
+    team: "GSW",
+    number: 30,
+    position: "G",
+    indicator: { height: "6-2", weight: 185 },
+    lastAttended: "Davidson",
+    country: "USA",
+  },
+  {
+    player: "Lebron James",
+    team: "LAL",
+    number: 6,
+    position: "F",
+    indicator: { height: "6-9", weight: 250 },
+    lastAttended: "St. Vincent-St. Mary HS (OH)",
+    country: "USA",
+  },
+  {
+    player: "Kevin Durant",
+    team: "BKN",
+    number: 7,
+    position: "F",
+    indicator: { height: "6-10", weight: 240 },
+    lastAttended: "Texas-Austin",
+    country: "USA",
+  },
+  {
+    player: "Giannis Antetokounmpo",
+    team: "MIL",
+    number: 34,
+    position: "F",
+    indicator: { height: "6-11", weight: 242 },
+    lastAttended: "Filathlitikos",
+    country: "Greece",
+  },
+];
 
 function showModal() {
   isShow.value = true;
@@ -132,6 +184,7 @@ onMounted(() => {
 });
 
 function handleTechSelect(tech) {
+  console.log(this.items);
   store.handleTechSelect(tech);
 }
 
