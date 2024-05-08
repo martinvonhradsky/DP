251d445d516bd199eaad3a38b5c101bfff628ecb
martinvonhradsky <martin.hradsky72@gmail.com>
2024-05-08 13:14:34 +0200

    registered vue table

--- client/src/components/TestPage.vue
+++ client/src/components/TestPage.vue
@@ -1,22 +1,73 @@
 <template>
   <div>
     <NavbarVue />
-    <CustomTest class="test" />
+    <EasyDataTable class="test" :headers="headers" :items="items" />
   </div>
 </template>
 
 <script>
 import NavbarVue from "./PageNavbar.vue";
-import CustomTest from "./CustomTest.vue";
+import Vue3EasyDataTable from "vue3-easy-data-table";
 
 export default {
   components: {
     NavbarVue,
-    CustomTest,
+    EasyDataTable: Vue3EasyDataTable,
+  },
+  data() {
+    return {
+      headers: [
+        { text: "PLAYER", value: "player" },
+        { text: "TEAM", value: "team" },
+        { text: "NUMBER", value: "number" },
+        { text: "POSITION", value: "position" },
+        { text: "HEIGHT", value: "indicator.height" },
+        { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
+        { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
+        { text: "COUNTRY", value: "country" },
+      ],
+      items: [
+        {
+          player: "Stephen Curry",
+          team: "GSW",
+          number: 30,
+          position: "G",
+          indicator: { height: "6-2", weight: 185 },
+          lastAttended: "Davidson",
+          country: "USA",
+        },
+        {
+          player: "Lebron James",
+          team: "LAL",
+          number: 6,
+          position: "F",
+          indicator: { height: "6-9", weight: 250 },
+          lastAttended: "St. Vincent-St. Mary HS (OH)",
+          country: "USA",
+        },
+        {
+          player: "Kevin Durant",
+          team: "BKN",
+          number: 7,
+          position: "F",
+          indicator: { height: "6-10", weight: 240 },
+          lastAttended: "Texas-Austin",
+          country: "USA",
+        },
+        {
+          player: "Giannis Antetokounmpo",
+          team: "MIL",
+          number: 34,
+          position: "F",
+          indicator: { height: "6-11", weight: 242 },
+          lastAttended: "Filathlitikos",
+          country: "Greece",
+        },
+      ],
+    };
   },
 };
 </script>
-
 <style scoped>
 .test {
   margin-top: 30px;
--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,12 @@ import "./style.css";
 import router from "./router.js";
 import axios from "axios";
 import { SERVER_IP } from "../config"; // Import the SERVER_IP constant
+
 import "vue-universal-modal/dist/index.css";
 import VueUniversalModal from "vue-universal-modal";
+
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
 axios.defaults.baseURL = SERVER_IP; // Set the default base URL for axios
 
@@ -19,4 +23,5 @@ app.use(pinia);
 app.use(VueUniversalModal, {
   teleportTarget: "#modals",
 });
+app.component("EasyDataTable", Vue3EasyDataTable);
 app.mount("#app");
